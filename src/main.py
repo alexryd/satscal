d60df20dec054422b4a8cfd150a8c9106c63fdfb
Alexander Ryden <alex@keyflow.se>
2012-02-11 17:56:35 +0100

    Add the ability to set process GID

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import grp
 import os
 import pwd
 import tornado.httpserver
@@ -9,6 +10,7 @@ from satscal import SATSCalApplication
 
 
 define('run_as_user', default=None, help='run as the given user (requires root privileges)')
+define('run_as_group', default=None, help='run as the given group (requires root privileges)')
 define('enable_xheaders', default=0, help='enable support for X-Real-Ip and X-Scheme headers', type=int)
 define('port', default=8080, help='run on the given port', type=int)
 define('child_processes', default=1, help='fork the given number of child processes', type=int)
@@ -17,6 +19,8 @@ define('child_processes', default=1, help='fork the given number of child proces
 def main():
     tornado.options.parse_command_line()
 
+    if options.run_as_group:
+        os.setgid(grp.getgrnam(options.run_as_group).gr_gid)
     if options.run_as_user:
         os.setuid(pwd.getpwnam(options.run_as_user).pw_uid)
 
