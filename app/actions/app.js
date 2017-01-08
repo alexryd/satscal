47cb8368dbb75ee4080e5aba0f0fed4ee8a5b92a
Alexander Ryden <alex.ryden@tictail.com>
2017-01-08 16:44:41 +0100

    Make the stored link state become stale after 24 hours

--- app/actions/app.js
+++ app/actions/app.js
@@ -2,6 +2,7 @@ import {Actions} from 'tide'
 import Immutable from 'immutable'
 
 const LINK_STATE_KEY = 'linkState'
+const LINK_STATE_TIMESTAMP_KEY = 'linkStateTimestamp'
 
 class AppActions extends Actions {
   storeLinkState() {
@@ -15,19 +16,25 @@ class AppActions extends Actions {
         LINK_STATE_KEY,
         JSON.stringify(linkState.toJS())
       )
+      window.sessionStorage.setItem(
+        LINK_STATE_TIMESTAMP_KEY,
+        (new Date).getTime().toString()
+      )
     } catch (e) {
     }
   }
 
   loadStoredLinkState() {
     let linkState = null
+    let timestamp = 0
 
     try {
       linkState = JSON.parse(window.sessionStorage.getItem(LINK_STATE_KEY))
+      timestamp = parseInt(window.sessionStorage.getItem(LINK_STATE_TIMESTAMP_KEY), 10)
     } catch (e) {
     }
 
-    if (linkState && linkState.token) {
+    if (linkState && linkState.token && ((new Date).getTime() - timestamp) < 24 * 60 * 60 * 1000) {
       this.updateState((state) => {
         return state
           .set('link', Immutable.fromJS(linkState))
